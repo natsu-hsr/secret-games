e3445911ee2d4c38164845c1599b513061e6585c
iappolonov <iappolonov@phoenixit.ru>
2025-06-13 13:29:04 +0300

    - fixed small bugs

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,7 +7,16 @@ import {logout} from '@store/slices/auth-slice';
 
 import styles from './header.module.scss';
 import LogoutSvg from "@assets/logout.svg?react";
-import logoImage from '@assets/logo.svg';
+import LogoSvg from '@assets/logo.svg?react';
+
+type NavMenu = {
+  title: string,
+  path: string,
+}
+const navMenus: NavMenu[] = [
+  {title: 'Домой', path: '/'},
+  {title: 'Задачи', path: '/tasks'}
+]
 
 export const Header = () => {
   const dispatch = useAppDispatch();
@@ -18,25 +27,23 @@ export const Header = () => {
     dispatch(logout())
   }
 
+  const handleTitleClick = () => {
+    navigate('/');
+  }
+
   return (
     <nav className={styles.header}>
-      <Flex align='center' gap={8}>
-        <img
-          className={styles.logo}
-          src={logoImage}
-          alt="Dice logo"
-        />
-        <div className={styles.title}>
-          ЛогистИгра
-        </div>
-      </Flex>
       <Flex
-        className={styles.menu}
+        className={styles['title-container']}
+        align='center'
+        gap={8}
+        onClick={handleTitleClick}
       >
-        {[
-          {title: 'Домой', path: '/'},
-          {title: 'Задачи', path: '/tasks'}
-        ].map(m => (
+        <LogoSvg className={styles.logo} />
+        <div className={styles.title}>ЛогистИгра</div>
+      </Flex>
+      <Flex className={styles.menu}>
+        {navMenus.map(m => (
           <div
             key={m.path}
             className={cn(styles.item, (location.pathname === m.path) && styles.active)}
--- src/components/task-content/components/custom-map/use-yandexmap-loader.ts
+++ src/components/task-content/components/custom-map/use-yandexmap-loader.ts
@@ -97,7 +97,6 @@ export const useYandexMapLoader = ({mapData, mapRef}: UseYandexMapLoaderArgs) =>
 
       map.geoObjects.add(placemark);
       geoObjectsRef.current.push(placemark);
-      // bounds.push(p.coordinates);
     });
 
     // автоцентрирование
